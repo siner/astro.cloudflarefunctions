2e64a54664be742be48e94767a7d1706beb5db75
Fran <fran.siner@gmail.com>
2022-08-22 17:02:32 +0200

    Testing environment variables

--- functions/edge/hello.js
+++ functions/edge/hello.js
@@ -9,8 +9,7 @@ export async function onRequest(context) {
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-  console.log(env);
-  const app_name = await env.astrocloudflaretests.get("APP_NAME");
+  const app_name = env.APP_NAME;
 
   return new Response(`Hello, ${app_name}!`);
 }
